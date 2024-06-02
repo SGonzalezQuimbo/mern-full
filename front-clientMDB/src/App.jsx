1068c63affc85dca2173f82d22afd0c72f2eb281
SGonzalezQuimbo <sebastiangonzalez4999@gmail.com>
2024-06-02 12:12:16 -0300

    Nuevas dependencias y views

--- front-clientMDB/src/App.jsx
+++ front-clientMDB/src/App.jsx
@@ -1,19 +1,25 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+
 import Card from './components/Card/Card'
 import './App.css'
 
 function App() {
 
+  const location = useLocation();
+
   return (
-    <>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-    </>
+    <div className='app'>
+    <h1>NavBar</h1>
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>}/>
+      <Route path='/login' element={<h1>Login</h1>}/>
+      <Route path='/register' element={<h1>Register</h1>}/>
+      <Route path='/tasks' element={<h1>Todas las tareas</h1>}/>
+      <Route path='/add-task' element={<h1>Agregar tarea</h1>}/>
+      <Route path='/detail-tasks/:id' element={<h1>Detalle de tareas</h1>}/>
+      <Route path='/profile' element={<h1>Perfil y opciones de perfil</h1>}/>
+    </Routes>
+    </div>
   )
 }
 
