4b5fea3834dc08724e033a6b2d582f3a5578619b
SGonzalezQuimbo <sebastiangonzalez4999@gmail.com>
2024-07-30 21:03:04 -0300

    Se cambio a una base de datos en la nube de AtlasMDB

--- back-serverMDB/src/db.js
+++ back-serverMDB/src/db.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
-const URL = 'mongodb://127.0.0.1:27017/firstcrud';
+//const URL = 'mongodb://127.0.0.1:27017/firstcrud';
+
+const URI = 'mongodb+srv://sebasmdb4999:sebasmdb4999@cluster0.8q8dhfh.mongodb.net/myfirstcrudmern?retryWrites=true&w=majority&appName=Cluster0'
 
 
 
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(URL);
-        console.log('DB is connected...');
+        await mongoose.connect(URI);
+        console.log('DB is connected on URL...');
     } catch (error) {
         console.log(error);
     }
